79034274310a2f0ba3c3f56985f2c037301bf13b
Nejc Dougan <nejc.dougan@flai.ai>
2024-11-16 15:37:30 +0100

    Fixed link to home page. Close nav bar on mobile after click

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <a href="/" class="header-logo">
+    <a href="/landing-page" class="header-logo">
       <img :src="logo" alt="Logo"/>
     </a>
     <div class="hamburger" @click="toggleMenu">
@@ -9,21 +9,25 @@
       <div></div>
     </div>
     <nav :class="{ active: isMenuOpen }">
-      <router-link to="/osgeo-konferenca">Konferenca</router-link>
-      <router-link to="/geodev">GeoDev</router-link>
-      <router-link to="/teren">Teren</router-link>
-      <router-link to="/about">O nas</router-link>
+      <router-link to="/osgeo-konferenca" @click="closeMenu">Konferenca</router-link>
+      <router-link to="/geodev" @click="closeMenu">GeoDev</router-link>
+      <router-link to="/teren" @click="closeMenu">Teren</router-link>
+      <router-link to="/about" @click="closeMenu">O nas</router-link>
     </nav>
   </header>
 </template>
 
 <script setup>
-import {ref} from 'vue';
+import { ref } from 'vue';
 import logo from '@/assets/logo-lg.svg'; // Import the logo image directly
 
 const isMenuOpen = ref(false);
 
 const toggleMenu = () => {
   isMenuOpen.value = !isMenuOpen.value;
+};
+
+const closeMenu = () => {
+  isMenuOpen.value = false;
 };
 </script>
